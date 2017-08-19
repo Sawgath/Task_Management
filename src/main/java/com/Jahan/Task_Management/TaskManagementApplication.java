a66ca44dcffc69893623c6d1e4f0dcf553c12b7c
Sawgath Jahan <sawjahan@gmail.com>
2017-08-20 04:03:39 +1000

    first commit prob

---
+++ src/main/java/com/Jahan/Task_Management/TaskManagementApplication.java
@@ -0,0 +1,12 @@
+package com.Jahan.Task_Management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskManagementApplication.class, args);
+	}
+}
