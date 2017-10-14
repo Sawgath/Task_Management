c4f2034bd92681b66f74f186cde9eb7849f54610
Sawgath Jahan <sawjahan@gmail.com>
2017-10-14 16:33:59 +1100

    25th commit-taskAssign,email send (not compeleted) aws deployed

--- src/main/java/com/Jahan/Task_Management/TaskManagementApplication.java
+++ src/main/java/com/Jahan/Task_Management/TaskManagementApplication.java
@@ -1,12 +1,25 @@
 package com.Jahan.Task_Management;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.web.support.SpringBootServletInitializer;;
 
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
 @SpringBootApplication
-public class TaskManagementApplication {
+public class TaskManagementApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TaskManagementApplication.class, args);
 	}
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	        return application.sources(TaskManagementApplication.class);
+	}
+
 }
